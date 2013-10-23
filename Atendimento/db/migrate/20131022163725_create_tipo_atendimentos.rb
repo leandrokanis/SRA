05337ab3b61d6859a7e121401da23fc112d26f7c
AlessandroCaetano <darkbubu123@gmail.com>
2013-10-23 15:56:44 -0200

    remoçao do atendimento feito anteriormente

--- Atendimento/db/migrate/20131022163725_create_tipo_atendimentos.rb
+++
@@ -1,9 +0,0 @@
-class CreateTipoAtendimentos < ActiveRecord::Migration
-  def change
-    create_table :tipo_atendimentos do |t|
-      t.string :tipo
-
-      t.timestamps
-    end
-  end
-end
--- Atendimento/test/unit/tipo_atendimento_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TipoAtendimentoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
