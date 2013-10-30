75ba49f0f0dc7be7bcaba907ab1d9d49742dc5ec
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2013-10-29 22:58:48 -0200

    Modificação nas Views

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SRA::Application.initialize!
+
+Time::DATE_FORMATS[:data] = "posted on %b, %m %Y - %I:%M%p"
