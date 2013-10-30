2dff871079b49a26a4ea92043f0d3baa283e5fac
suzukijessica <suzukijessica@localhost.localdomain>
2013-10-30 12:21:07 -0200

    Mensagem de erro sem o atributo

--- app/models/atendimento.rb
+++ app/models/atendimento.rb
@@ -3,7 +3,7 @@ class Atendimento < ActiveRecord::Base
   belongs_to :place
   attr_accessible :data, :pessoa_id, :place_id
 
-  validates :place, :presence => true, :length => {:minimum => 3}
+  validates_presence_of :place
   validates_presence_of :pessoa
   
 end
