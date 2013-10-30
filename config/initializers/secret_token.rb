affb3b3b61da7495ab8c1f020202483d7c8252f5
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2013-10-30 09:45:12 -0200

    Secret Token tentativa de reenvio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SRA::Application.config.secret_token = 'adb5d83d4c268cee7eb753315d1363e901c50c02f9901ab8fade91a5e099288f00393614311cbb6a0b4aefcaad0ace4541ef7f35b8bd867504a02e03d2be9128'
