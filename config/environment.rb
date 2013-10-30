0abd941a49b4636f0d8c5c45fa90af9450ffc051
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2013-10-30 09:38:19 -0200

    Secret Token adicionando

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 SRA::Application.initialize!
 
-Time::DATE_FORMATS[:data] = "posted on %b, %m %Y - %I:%M%p"
+
+
+Time::DATE_FORMATS[:data] = "%bb, %m %Y - %I:%M%p"
