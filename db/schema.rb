1da831a533493037c0c0e48892cc89b40e214b29
thiagovsk <thiagitosouza@hotmail.com>
2013-10-31 10:54:16 -0200

    Corrigindo bug da view mostrando o id da combo e nao o conteudo

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(:version => 20131029175655) do
   end
 
   create_table "professors", :force => true do |t|
-    t.string   "matricula"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
