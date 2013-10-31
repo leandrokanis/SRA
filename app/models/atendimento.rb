75e007c30a8c3947c67a3843c2c9fe99a41c0f6f
thiagovsk <thiagitosouza@hotmail.com>
2013-10-31 12:08:24 -0200

    Adicionando botões  para locais de atendimento

--- app/models/atendimento.rb
+++ app/models/atendimento.rb
@@ -3,7 +3,7 @@ class Atendimento < ActiveRecord::Base
   belongs_to :place
   attr_accessible :data, :pessoa_id, :place_id
 
-  validates_presence_of :place
+  validates_presence_of :place 
   validates_presence_of :pessoa
   
 end
