9805e39f1d2c8c244e7d6e4bf2fe0d2e2d3ee60d
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2013-11-02 18:16:37 -0200

    Modificação do Formulario com Data Correta

--- app/models/atendimento.rb
+++ app/models/atendimento.rb
@@ -2,7 +2,7 @@ class Atendimento < ActiveRecord::Base
   belongs_to :pessoa
   belongs_to :place
   belongs_to  :type
-  attr_accessible :data, :pessoa_id, :place_id, :type_id
+  attr_accessible :data, :pessoa_id, :pessoa, :place_id, :type_id
 
 
   validates_presence_of :place 
