311c46aea0c4cce932963b5afb92f64b8bd16885
Jessica <suzuki_jessica@hotmail.com>
2013-11-18 17:39:50 -0200

    Validando campos que permitiam numeros negativos ou letras em campos numericos

--- app/models/comunidade.rb
+++ app/models/comunidade.rb
@@ -4,5 +4,7 @@ class Comunidade < ActiveRecord::Base
   acts_as :pessoa
   attr_accessible :identificacao
   validates_uniqueness_of :identificacao
+  validates_numericality_of :identificacao , greater_than: 0
+
 
 end
