f4a3011e80e27df65753bdea6bd3a2746be60f9f
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2014-01-29 11:23:49 -0200

    Modificações no schema

---
+++ db/schema.rb
@@ -0,0 +1,87 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20140129041055) do
+
+  create_table "alunos", :force => true do |t|
+    t.string   "matricula"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "atendimentos", :force => true do |t|
+    t.integer  "user_id"
+    t.datetime "data"
+    t.integer  "place_id"
+    t.integer  "type_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "atendimentos", ["place_id"], :name => "index_atendimentos_on_place_id"
+  add_index "atendimentos", ["type_id"], :name => "index_atendimentos_on_type_id"
+  add_index "atendimentos", ["user_id"], :name => "index_atendimentos_on_user_id"
+
+  create_table "places", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "places_types", :id => false, :force => true do |t|
+    t.integer "place_id"
+    t.integer "type_id"
+  end
+
+  add_index "places_types", ["place_id", "type_id"], :name => "index_places_types_on_place_id_and_type_id"
+
+  create_table "professors", :force => true do |t|
+    t.string   "matricula"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "servidors", :force => true do |t|
+    t.string   "matricula"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "types", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.integer  "as_user_id"
+    t.string   "as_user_type"
+    t.string   "name"
+    t.string   "encrypted_password"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0, :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
+    t.string   "username"
+  end
+
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
+
+end
