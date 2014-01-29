96876d453ea7a4350483ec46dcb4950257020848
thaigovsk <thiagitosouza@hotmail.com>
2014-01-29 15:29:56 -0200

    Adicionando testes funcionais de aluno

--- test/functional/waitir/professor.rb
+++ test/functional/waitir/professor.rb
@@ -3,11 +3,16 @@ require 'watir-webdriver'
 
 #Cadastrar, alterar e excluir Professor
 browser = Watir::Browser.start "http://localhost:3000/professors"
-browser.link(:text, "Criar Professor").click
-browser.text_field(:id, "professor_name").set("Hilmer")
-browser.text_field(:id, "professor_matricula").set("5647")
-browser.button(:value,"Criar Professor").click
-browser.link(:text, "Editar").click
+#Criação de Professor
+browser.text_field(:id, "user_username").set("thaiane")
+browser.text_field(:id, "user_password").set("123456")
+browser.button(:value,"Login").click
+
+browser.goto "http://localhost:3000/professors/new"
+
 browser.text_field(:id, "professor_name").set("Hilmer Neri")
-browser.button(:value,"Atualizar Professor").click
-browser.link(:text, "Deletar").click
+browser.text_field(:id, "professor_matricula").set("5647")
+browser.text_field(:id, "professor_username").set("hilmer.neri")
+browser.text_field(:id, "professor_password").set("123456")
+browser.text_field(:id, "professor_password_confirmation").set("123456")
+browser.button(:value,"Criar Professor").click
