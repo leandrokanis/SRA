b5a59bd3b41eb71c22267a1d161fa6c2de52bab8
thaigovsk <thiagitosouza@hotmail.com>
2014-01-30 12:22:03 -0200

    corrigindo teste de aluno e adicionando botao voltar com path correto

--- test/functional/waitir/aluno.rb
+++ test/functional/waitir/aluno.rb
@@ -4,55 +4,41 @@ require 'watir-webdriver'
 #Cadastrar aluno
 #browser = Watir::Browser.start "http://localhost:3000"
 
-
+#registro de aluno com sucesso
 browser = Watir::Browser.new
 browser.goto "http://localhost:3000"
 
-browser.link(:text, "Ainda não tem cadastro?").click
-browser.text_field(:id, "aluno_name").set("Thaiane")
-browser.text_field(:id, "aluno_matricula").set("120136198")
-browser.text_field(:id, "aluno_username").set("thaiane")
-browser.text_field(:id, "aluno_password").set("123456")
-browser.text_field(:id, "aluno_password_confirmation").set("123456")
-browser.button(:value,"Criar Aluno").click
 
+browser.link(:text, "Ainda não tem cadastro?").click
+browser.text_field(:id, "aluno_name").set("thiago")
+browser.text_field(:id, "aluno_matricula").set("100125271")
+browser.text_field(:id, "aluno_username").set("thiago")
+browser.text_field(:id, "aluno_password").set("12345678")
+browser.text_field(:id, "aluno_password_confirmation").set("12345678")
+browser.button(:id,"cadastrar-aluno").click
 
 browser.goto "http://localhost:3000"
 
 
 # Cadastrar usuário já existente
-browser.link(:text, "Ainda não tem cadastro?").click
-browser.text_field(:id, "aluno_name").set("thaiane")
-browser.text_field(:id, "aluno_matricula").set("120136198")
-browser.text_field(:id, "aluno_username").set("thaiane")
-browser.text_field(:id, "aluno_name").set("Thaiane")
-browser.text_field(:id, "aluno_password").set("123456")
-browser.text_field(:id, "aluno_password_confirmation").set("123456")
-browser.button(:value,"Criar Aluno").click
 
-browser.goto "http://localhost:3000"
+browser.text_field(:id, "aluno_name").set("thiago")
+browser.text_field(:id, "aluno_matricula").set("100125271")
+browser.text_field(:id, "aluno_username").set("thiago")
+browser.text_field(:id, "aluno_password").set("12345678")
+browser.text_field(:id, "aluno_password_confirmation").set("12345678")
+browser.button(:id,"cadastrar-aluno").click
 
 
 # Senha inválida
-browser.text_field(:id, "user_username").set("thaiane")
-browser.text_field(:id, "user_password").set("12345")
-browser.button(:value,"Login").click
+browser.text_field(:id, "user_username").set("thiagovsk")
+browser.text_field(:id, "user_password").set("1010101010")
+browser.button(:value,"Login ").click
 
 # Login inválido
-browser.text_field(:id, "user_username").set("thaian")
-browser.text_field(:id, "user_password").set("123456")
-browser.button(:value,"Login").click
 
 
 
 # Realizar atendimento
-browser.text_field(:id, "user_username").set("thaiane")
-browser.text_field(:id, "user_password").set("123456")
-browser.button(:value,"Login").click
-browser.select_list(:id, "atendimento_place_id").select("Secretaria da Direção")
-browser.select_list(:id, "atendimento_type_id").select("Declaração")
-browser.button(:value,"Criar Atendimento").click
-browser.link(:text, "Sair").click
-
 
 browser = Watir::Browser.start "http://localhost:3000"
