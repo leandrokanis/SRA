358c7e68aad4a112adb605e1eff43f362ee42b6d
thaigovsk <thiagitosouza@hotmail.com>
2014-01-30 12:39:12 -0200

    idicionando ID's aos botoes css

--- test/functional/waitir/aluno.rb
+++ test/functional/waitir/aluno.rb
@@ -8,7 +8,6 @@ require 'watir-webdriver'
 browser = Watir::Browser.new
 browser.goto "http://localhost:3000"
 
-
 browser.link(:text, "Ainda não tem cadastro?").click
 browser.text_field(:id, "aluno_name").set("thiago")
 browser.text_field(:id, "aluno_matricula").set("100125271")
@@ -21,7 +20,7 @@ browser.goto "http://localhost:3000"
 
 
 # Cadastrar usuário já existente
-
+browser.link(:text, "Ainda não tem cadastro?").click
 browser.text_field(:id, "aluno_name").set("thiago")
 browser.text_field(:id, "aluno_matricula").set("100125271")
 browser.text_field(:id, "aluno_username").set("thiago")
@@ -31,14 +30,26 @@ browser.button(:id,"cadastrar-aluno").click
 
 
 # Senha inválida
+browser.goto "http://localhost:3000"
 browser.text_field(:id, "user_username").set("thiagovsk")
 browser.text_field(:id, "user_password").set("1010101010")
-browser.button(:value,"Login ").click
+browser.button(:id,"login").click
 
 # Login inválido
+browser.text_field(:id, "user_username").set("araujo")
+browser.text_field(:id, "user_password").set("1010101010")
+browser.button(:id,"login").click
 
 
 
 # Realizar atendimento
 
+
+browser.text_field(:id, "user_username").set("thiago")
+browser.text_field(:id, "user_password").set("12345678")
+browser.button(:id,"login").click
+browser.select_list(:id, "atendimento_place_id").select("Secretaria de Graduação")
+browser.select_list(:id, "atendimento_type_id").select("Concursos")
+browser.button(:id,"criar-atendimento").click
+browser.button(:id,"logout").click
 browser = Watir::Browser.start "http://localhost:3000"
