cfa591e2960b62238ddda4b31bbf70b18ff78369
AlessandroCaetano <darkbubu123@gmail.com>
2014-01-30 10:28:20 -0200

    Alterando permissões de Servidor e Aluno

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,16 +5,11 @@ class Ability
     user ||= User.new # guest user
 
     if user.as_user_type ==  'Aluno'
-      can :manage, :all
+      can :create, Atendimento
     elsif user.as_user_type == 'Professor'
       can :create, Atendimento
     elsif user.as_user_type == 'Servidor'
-      can :manage, Type
-      can :manage, Place
-      can :manage, Servidor
-      can :manage, Professor
-      can :read, :all
-      can [:edit, :update], Atendimento
+      can [:read, :edit, :create, :update], [Atendimento,Type, Professor, Aluno, Place, Servidor]
     end
 
   end
